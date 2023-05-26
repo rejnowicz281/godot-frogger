27e64b543e07643a8cda0a8f87f4c843a3627bf7
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-05-26 13:14:46 +0200

    Add game over screen

--- Car/car.gd
+++ Car/car.gd
@@ -5,3 +5,7 @@ var speed = 250
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x -= speed * delta
+
+
+func _on_body_entered(body):
+	get_tree().change_scene_to_file("res://OverScreen/over_screen.tscn")

85bf585dac614c30a681d69985fed97eb42abd70
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-05-26 15:11:56 +0200

    Add more car starting positions

--- Level/level.gd
+++ Level/level.gd
@@ -2,7 +2,22 @@ extends Node
 
 const CAR = preload("res://Car/car.tscn")
 
-func _on_timer_timeout():
+func spawn_car(car_start_id):
 	var car = CAR.instantiate()
-	car.position = $"CarStart1".position
+	var car_start_path = "StartPositions/CarStart" + str(car_start_id)
+	car.position = get_node(car_start_path).position
 	add_child(car)
+
+func _on_timer_1_timeout(): spawn_car(1)
+
+
+func _on_timer_2_timeout(): spawn_car(2)
+
+
+func _on_timer_3_timeout(): spawn_car(3)
+
+
+func _on_timer_4_timeout(): spawn_car(4)
+
+
+func _on_timer_5_timeout(): spawn_car(5)
